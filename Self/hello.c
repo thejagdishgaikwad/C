317a8f7ad7e2840e3f5d4f906304f3b0099adde5
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2024-10-25 20:39:00 +0530

    Rm some of the unnecessary codes!

--- Self/hello.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-    int main(){
-        printf("Hello World\n");
-        printf("Hello World\n");
-        printf("Hello World\n");
-        printf("Hello World\n"); //this is new line function
-        return 0;
-    }
